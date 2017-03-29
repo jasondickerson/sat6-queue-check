059e5c6b933a990d7c87eb90c58b2a6f9707c7e8
jasondickerson <jason.dickerson@gmail.com>
2017-03-29 17:49:58 -0500

    Update sat6-queue-check.bash

--- sat6-queue-check.bash
+++ sat6-queue-check.bash
@@ -1,10 +1,10 @@
 #!/bin/bash
-if [ ! -f /tmp/qpid_search ] ; then
+if [ ! -f qpid_search ] ; then
         echo """queue
 celery
 pulp
 resource
-=""" > /tmp/qpid_search
+=""" > qpid_search
 fi
 
 echo -e "\e[1;41;33mUptime and Load Average:\e[0m"
@@ -13,14 +13,23 @@ echo
 echo -e "\e[1;41;33mPassenger Status\e[0m"
 passenger-status | head -n 12
 echo
+echo -en "\e[1;41;33mForeman Task Queue:\e[0m  "
+foreman-rake foreman_tasks:cleanup TASK_SEARCH='label ~ *' STATES=running,pending,planning,planned VERBOSE=true NOOP=true 2> /dev/null | grep deleted |cut -d\  -f2
+echo
 echo -en "\e[1;41;33mMonitor Event Queue Task backlog:\e[0m  "
 echo "select count(*) from katello_events" | su - postgres -c "psql foreman"|head -n3|tail -n1|tr -s " "|cut -d\  -f2
 echo
 echo -en "\e[1;41;33mListen on candlepin events Task backlog:\e[0m  "
 qpid-stat --ssl-certificate /etc/pki/katello/certs/java-client.crt --ssl-key /etc/pki/katello/private/java-client.key -b "amqps://localhost:5671" -q katello_event_queue|grep queue-depth|tr -s " "|cut -d\  -f3
 echo
-echo -en "\e[1;41;33mSatellite QPID\e[0m "
-qpid-stat -q --ssl-certificate=/etc/pki/katello/qpid_client_striped.crt -b amqps://localhost:5671 | grep -v pulp.agent | grep -if /tmp/qpid_search
-echo
 echo -e "\e[1;41;33mPulp Tasks Count by State:\e[0m"
-pulp-admin tasks list |grep State|uniq -c
+PULP_RESULT=$(pulp-admin tasks list |grep State|uniq -c)
+if [ -n "${PULP_RESULT}" ] ; then
+        echo ${PULP_RESULT}
+else
+        echo "No Pulp Tasks Running."
+fi
+echo
+echo -en "\e[1;41;33mSatellite QPID\e[0m "
+qpid-stat -q --ssl-certificate=/etc/pki/katello/qpid_client_striped.crt -b amqps://localhost:5671 | grep -v pulp.agent | grep -if qpid_search
+echo
