7fbcc663acaef87b572d20dc47af571d8f14d446
Jason Dickerson <jason.dickerson@gmail.com>
2018-06-01 18:24:01 -0500

    Added the ability for the script to detect if running on satellite or not.  If not on satellite, assume running on capsule and act accordingly.

--- sat6-queue-check.bash
+++ sat6-queue-check.bash
@@ -13,15 +13,21 @@ echo
 echo -e "\e[1;41;33mPassenger Status\e[0m"
 passenger-status | head -n 13
 echo
-echo -en "\e[1;41;33mForeman Task Queue:\e[0m  "
-foreman-rake foreman_tasks:cleanup TASK_SEARCH='label ~ *' STATES=running,pending,planning,planned VERBOSE=true NOOP=true 2> /dev/null | grep "tasks would be deleted" |cut -d\  -f2
-echo
-echo -en "\e[1;41;33mMonitor Event Queue Task backlog:\e[0m  "
-echo "select count(*) from katello_events" | su - postgres -c "psql foreman"|head -n3|tail -n1|tr -s " "|cut -d\  -f2
-echo
-echo -en "\e[1;41;33mListen on candlepin events Task backlog:\e[0m  "
-qpid-stat --ssl-certificate /etc/pki/katello/certs/java-client.crt --ssl-key /etc/pki/katello/private/java-client.key -b "amqps://localhost:5671" -q katello_event_queue|grep queue-depth|tr -s " "|cut -d\  -f3
-echo
+
+rpm -q satellite > /dev/null
+IS_SATELLITE=$?
+if [ ${IS_SATELLITE} -eq 0 ] ; then 
+	echo -en "\e[1;41;33mForeman Task Queue:\e[0m  "
+	foreman-rake foreman_tasks:cleanup TASK_SEARCH='label ~ *' STATES=running,pending,planning,planned VERBOSE=true NOOP=true 2> /dev/null | grep "tasks would be deleted" |cut -d\  -f2
+	echo
+	echo -en "\e[1;41;33mMonitor Event Queue Task backlog:\e[0m  "
+	echo "select count(*) from katello_events" | su - postgres -c "psql foreman"|head -n3|tail -n1|tr -s " "|cut -d\  -f2
+	echo
+	echo -en "\e[1;41;33mListen on candlepin events Task backlog:\e[0m  "
+	qpid-stat --ssl-certificate /etc/pki/katello/certs/java-client.crt --ssl-key /etc/pki/katello/private/java-client.key -b "amqps://localhost:5671" -q katello_event_queue|grep queue-depth|tr -s " "|cut -d\  -f3
+	echo
+fi
+
 echo -e "\e[1;41;33mPulp Tasks Count by State:\e[0m"
 PULP_RESULT=$(pulp-admin tasks list|grep Operations|sort|uniq -c ; echo ; pulp-admin tasks list|grep State|sort |uniq -c)
 if [ -n "${PULP_RESULT}" ] ; then
@@ -38,6 +44,9 @@ qpid-stat -q --ssl-certificate=/etc/pki/katello/qpid_client_striped.crt -b amqps
 echo
 echo -en "\e[1;41;33mSatellite Service Status:\e[0m  "
 katello-service status 2> /dev/null|tail -n1
-echo
-echo -e "\e[1;41;33mHammer Ping Results:\e[0m  "
-hammer ping
+
+if [ ${IS_SATELLITE} -eq 0 ] ; then
+	echo
+	echo -e "\e[1;41;33mHammer Ping Results:\e[0m  "
+	hammer ping
+fi
